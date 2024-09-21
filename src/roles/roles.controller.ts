76f7b6eb77dc06144a412fbbd82fa341e6052f8d
lukamrko <lukamrko@gmail.com>
2024-09-21 19:00:10 +0200

    Updated the way that controller works, to show codes and stuff

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('roles')
 export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
+  constructor(private readonly rolesService: RolesService) { }
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
+  @HttpCode(HttpStatus.CREATED) // Returns 201 Created on success
+  async create(@Body() createRoleDto: CreateRoleDto) {
+    const role = await this.rolesService.create(createRoleDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Role successfully created',
+      data: role,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  @HttpCode(HttpStatus.OK) // Returns 200 OK on success
+  async findAll() {
+    const roles = await this.rolesService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Roles retrieved successfully',
+      data: roles,
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  @HttpCode(HttpStatus.OK) // Returns 200 OK on success
+  async findOne(@Param('id') id: string) {
+    const role = await this.rolesService.findOne(+id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Role with ID ${id} retrieved successfully`,
+      data: role,
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  @HttpCode(HttpStatus.OK) // Returns 200 OK on success
+  async update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+    const updatedRole = await this.rolesService.update(+id, updateRoleDto);
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Role with ID ${id} updated successfully`,
+      data: updatedRole,
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT) // Returns 204 No Content on success
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    try {
+      await this.rolesService.remove(+id);
+      res.status(HttpStatus.NO_CONTENT).json({
+        statusCode: HttpStatus.NO_CONTENT,
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: error.message,
+      });
+    }
   }
 }
