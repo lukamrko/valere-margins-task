ac12520ad6805790ad8738ea3441eddae5a9a70d
lukamrko <lukamrko@gmail.com>
2024-09-21 18:41:31 +0200

    Role API works. Some weird behaviour needs to fixed

---
+++ src/roles/dto/return-role.dto.ts
@@ -0,0 +1,4 @@
+export class ReturnRoleDto {
+    roleID: number;
+    description: string;
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { ReturnRoleDto } from './dto/return-role.dto';
+import { Role } from './entities/role.entity';
+
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) { }
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const newRole = this.roleRepository.create(createRoleDto);
+    return await this.roleRepository.save(newRole);
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(): Promise<ReturnRoleDto[]> {
+    const roles = await this.roleRepository.find();
+    return roles.map(role => ({
+      roleID: role.roleID,
+      description: role.description,
+    }));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number): Promise<ReturnRoleDto> {
+    const role = await this.roleRepository.findOne({ where: { roleID: id } });
+    if (role) {
+      return { roleID: role.roleID, description: role.description };
+    }
+    return null;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = await this.roleRepository.findOne({ where: { roleID: id } });
+    if (role) {
+      Object.assign(role, updateRoleDto);
+      return await this.roleRepository.save(role);
+    }
+    //TODO handle this case
+    return null;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number): Promise<void> {
+    await this.roleRepository.delete(id);
   }
 }
