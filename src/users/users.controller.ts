bcf00a3d22c449633ba1ac441b6818d15fef2302
lukamrko <lukamrko@gmail.com>
2024-09-22 18:45:00 +0200

    Roles on class level

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,11 @@ import { Role } from '../config/role.enums';
 
 
 @UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.Admin)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
-  @Roles(Role.Admin)
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     const user = await this.usersService.create(createUserDto);
@@ -24,7 +24,6 @@ export class UsersController {
     };
   }
 
-  @Roles(Role.Admin)
   @Get()
   async findAll() {
     const users = await this.usersService.findAll();
@@ -35,7 +34,6 @@ export class UsersController {
     };
   }
 
-  @Roles(Role.Admin)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
@@ -46,7 +44,6 @@ export class UsersController {
     };
   }
 
-  @Roles(Role.Admin)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const updatedUser = await this.usersService.update(+id, updateUserDto);
@@ -57,7 +54,6 @@ export class UsersController {
     };
   }
 
-  @Roles(Role.Admin)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     await this.usersService.remove(+id);
