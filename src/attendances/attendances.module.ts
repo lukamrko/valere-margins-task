728b1c4e739cd164cd0a139eeea90a314d626dc7
lukamrko <lukamrko@gmail.com>
2024-09-22 21:49:56 +0200

    Update on attendanceModule

--- src/attendances/attendances.module.ts
+++ src/attendances/attendances.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttendancesService } from './attendances.service';
 import { AttendancesController } from './attendances.controller';
+import { Attendance } from './entities/attendance.entity';
+import { UsersModule } from 'src/users/users.module';
+import { ClassesModule } from 'src/classes/classes.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Attendance]),
+    UsersModule,
+    ClassesModule],
   controllers: [AttendancesController],
   providers: [AttendancesService],
+  exports: [AttendancesService],
 })
 export class AttendancesModule {}
