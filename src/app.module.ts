951e39d018b54b1e3346a91efd4f472ab7fb9ef3
lukamrko <lukamrko@gmail.com>
2024-09-25 11:30:12 +0200

    better db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
@@ -13,11 +13,16 @@ import { ClassesModule } from './classes/classes.module';
 import { SchedulesModule } from './schedules/schedules.module';
 import { AttendancesModule } from './attendances/attendances.module';
 
-
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(databaseConfig()),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => databaseConfig(configService),
+    }),
     UsersModule,
     AuthModule,
     SportsModule,
@@ -29,7 +34,6 @@ import { AttendancesModule } from './attendances/attendances.module';
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {
   constructor(private dataSource: DataSource) { }
 }
--- src/database-config/database.config.ts
+++ src/database-config/database.config.ts
@@ -1,15 +1,30 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from "../users/entities/user.entity";
-import { Sport } from "../sports/entities/sport.entity";
-import { Week } from "../weeks/entities/week.entity";
-import { Class } from "../classes/entities/class.entity";
-import { Schedule } from "../schedules/entities/schedule.entity";
-import { Attendance } from "../attendances/entities/attendance.entity";
+import { ConfigService } from '@nestjs/config';
+import { User } from '../users/entities/user.entity';
+import { Sport } from '../sports/entities/sport.entity';
+import { Week } from '../weeks/entities/week.entity';
+import { Class } from '../classes/entities/class.entity';
+import { Schedule } from '../schedules/entities/schedule.entity';
+import { Attendance } from '../attendances/entities/attendance.entity';
 
+const validDbTypes = ['mysql', 'mariadb', 'postgres', 'sqlite', 'mssql'] as const;
+type DbType = typeof validDbTypes[number];
 
-export const databaseConfig = (): TypeOrmModuleOptions => ({
-    type: 'sqlite',
-    database: 'sport-complex-db.db',
-    entities: [User, Sport, Week, Class, Schedule, Attendance],
-    synchronize: true,
-});
+function isValidDbType(type: string): type is DbType {
+    return (validDbTypes as readonly string[]).includes(type);
+}
+
+export const databaseConfig = (configService: ConfigService): TypeOrmModuleOptions => {
+    const dbType = configService.get<string>('DB_TYPE', 'sqlite');
+
+    if (!isValidDbType(dbType)) {
+        throw new Error(`Invalid DB_TYPE: ${dbType}`);
+    }
+
+    return {
+        type: dbType, 
+        database: configService.get<string>('DB_NAME', 'sport-complex-db.db'),
+        entities: [User, Sport, Week, Class, Schedule, Attendance],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE', true),
+    };
+};
